3e543bb093d9834fa125b0071137cc43485fe713
brundagejoe <joebrundage@icloud.com>
2024-08-22 21:30:51 -0600

    fix basename

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: "/seoreo/",
   plugins: [
     remix({
+      basename: "/seoreo/",
       ssr: false,
       future: {
         v3_fetcherPersist: true,
